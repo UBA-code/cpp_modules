2dabeb7cb8eeed49605db7d709a89a80768ddad6
Yassine Bel Hachmi <ybel-hac@e1r13p1.1337.ma>
2023-07-18 08:55:37 +0100

    push to laptop

---
+++ day_06/ex01/Serializer.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include "Data.hpp"
+#include "cstdint"
+
+class Serializer {
+	private:
+		Serializer();
+		Serializer(Serializer& obj);
+		Serializer& operator=(Serializer& obj);
+	public:
+		static uintptr_t serialize(Data* ptr);
+		static Data* deserialize(uintptr_t raw);
+};
--- day_06/ex01/main.cpp
+++ day_06/ex01/main.cpp
@@ -1,4 +1,5 @@
 #include "iostream"
+#include "Serializer.hpp"
 
 void	test1()
 {
@@ -25,7 +26,7 @@ int main (int ac, char **av)
 {
 	if (ac == 2)
 	{
-		// ScalarConverter::convert(av[1]);
+		
 	}
 
 	return (0);
